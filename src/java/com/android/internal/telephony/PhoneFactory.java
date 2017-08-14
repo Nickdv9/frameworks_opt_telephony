LineageOS/android_frameworks_opt_telephony

telephony: Override nw mode on creation and when trying to calculate nw type.
Change-Id: Ic97ade9486c2af8fb2ecbbdcba0eb58f69a3c925

src/java/com/android/internal/telephony/PhoneFactory.java
@@ -233,6 +233,13 @@ public class PhoneFactory {
                 // because ImsService might need it when it is being opened.
                 for (int i = 0; i < numPhones; i++) {
                     sProxyPhones[i].startMonitoringImsService();
+                    // Get users NW type, let it override if its not the default NW mode (-1)
+                    int userNwType = SubscriptionController.getInstance().getUserNwMode(
+                            sProxyPhones[i].getSubId());
+                    if (userNwType != SubscriptionManager.DEFAULT_NW_MODE
+                            && userNwType != networkModes[i]) {
+                        sProxyPhones[i].setPreferredNetworkType(userNwType, null);
+                    }
                 }
             }
         }
@@ -370,6 +377,14 @@ public class PhoneFactory {
             networkType = android.provider.Settings.Global.getInt(context.getContentResolver(),
                     android.provider.Settings.Global.PREFERRED_NETWORK_MODE + phoneSubId,
                     phoneIdNetworkType);
+
+            // Get users NW type, let it override if its not the default NW mode (-1)
+            int userNwType = SubscriptionController.getInstance().getUserNwMode(phoneSubId);
+            if (userNwType != SubscriptionManager.DEFAULT_NW_MODE && userNwType != networkType) {
+                Rlog.d(LOG_TAG, "calculatePreferredNetworkType: overriding for usernw mode " +
+                        "phoneSubId = " + phoneSubId + " networkType = " + networkType);
+                networkType = userNwType;
+            }
         } else {
             Rlog.d(LOG_TAG, "calculatePreferredNetworkType: phoneSubId = " + phoneSubId +
                     " is not a active SubId");

Nickdv9/frameworks_opt_telephony

src/java/com/android/internal/telephony/PhoneFactory.java
@@ -235,6 +235,13 @@ public class PhoneFactory {
                 // because ImsService might need it when it is being opened.
                 for (int i = 0; i < numPhones; i++) {
                     sPhones[i].startMonitoringImsService();
+                    // Get users NW type, let it override if its not the default NW mode (-1)
+                    int userNwType = SubscriptionController.getInstance().getUserNwMode(
+                            sPhones[i].getSubId());
+                    if (userNwType != SubscriptionManager.DEFAULT_NW_MODE
+                            && userNwType != networkModes[i]) {
+                        sPhones[i].setPreferredNetworkType(userNwType, null);
+                    }
                 }
 
                 ITelephonyRegistry tr = ITelephonyRegistry.Stub.asInterface(
@@ -337,6 +344,14 @@ public class PhoneFactory {
         int networkType = android.provider.Settings.Global.getInt(context.getContentResolver(),
                 android.provider.Settings.Global.PREFERRED_NETWORK_MODE + phoneSubId,
                 RILConstants.PREFERRED_NETWORK_MODE);
+
+        // Get users NW type, let it override if its not the default NW mode (-1)
+        int userNwType = SubscriptionController.getInstance().getUserNwMode(phoneSubId);
+        if (userNwType != SubscriptionManager.DEFAULT_NW_MODE && userNwType != networkType) {
+            Rlog.d(LOG_TAG, "calculatePreferredNetworkType: overriding for usernw mode " +
+                    "phoneSubId = " + phoneSubId + " networkType = " + networkType);
+            networkType = userNwType;
+        }
         Rlog.d(LOG_TAG, "calculatePreferredNetworkType: phoneSubId = " + phoneSubId +
                 " networkType = " + networkType);
         return networkType;